bce9b4f1f34c85e1bd97e0b51ca97aabbc6a9168
RAJAGOPAL SRINIVASAN <raj.70.srinivasan@gmail.com>
2015-03-02 15:09:37 -0600

    Week4
    Assignment

---
+++ Week-4 Assignment/orgchart-with supervisor and reviewer in a single query.sql
@@ -0,0 +1,5 @@
+﻿SELECT a.employeename as EMPLOYEE,b.employeename as SUPERVISOR,c.employeename as REVIEWER
+  FROM orgchar	t a, orgchart b, orgchart c
+  where a.supervisorid = b.employeeid
+  and   a.reviewerid = c.employeeid
+  order by a.employeeid
