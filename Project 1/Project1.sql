4b0da9d67c7bcc16c1e27aa71ee87dba9fe006e8
RAJAGOPAL SRINIVASAN <raj.70.srinivasan@gmail.com>
2015-03-02 15:13:26 -0600

    Week4 Project1
    Week4 Project1

---
+++ Project 1/Project1.sql
@@ -0,0 +1,8 @@
+﻿-- The select query combining three different tables which includes a NULL column in studentID
+
+SELECT a.*,c.firstname,c.middlename,c.lastname
+  FROM examresult a,
+       roomstudent b,
+       studentmaster c
+  where a.studentid = b.studentid
+  and   a.studentid = c.studentid;
